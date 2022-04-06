a360edaf0bf030c01b38d8651630cadc36f9a08d
Jason Cox <dev@jasoncarloscox.com>
2022-04-06 12:58:33 -0600

    Add script and readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# LDS General Conference for e-readers
+
+A script to convert LDS General Conference to EPUB, as well as converted files for past conferences.
+
+These are pretty basic EPUBs -- the footnotes are removed, images may not show up right, and links aren't guaranteed to work -- but if you just want a table of contents and the text of each talk, they should be good enough.
+
+## Gimme the files
+
+If you just want to download the files for your e-reader, check out the `conferences/` directory. Currently `.epub` and `.mobi` files are provided, as those are what my wife and I need for our e-readers.
+
+If the format you need isn't there, try using a tool like [Calibre](https://calibre-ebook.com/) (desktop application that also ships with the `ebook-convert` CLI) or [Cloud Convert](https://cloudconvert.com/epub-converter) (online converter) to convert the `.epub` file to the format you need.
+
+## I want to use the script
+
+### Requirements
+
+The Bash script depends on [curl](https://curl.sh), [htmlq](https://github.com/mgdm/htmlq) and [pandoc](https://pandoc.org).
+
+### Usage
+
+`conf-to-epub.sh <year> <month> [<language>]`
+
+- `<year>` is 4 digits
+- `<month>` is one of `april`, `apr`, `4`, `04`, `october`, `oct`, `10` (case-insensitive)
+- `<language>` (optional) is the argument to the `lang` query parameter on the `churchofjesuschrist.org` URLs and defaults to `eng`
+
+## I want to get in touch
+
+If you have questions, suggestions, etc., you can open a GitHub issue or contact me at [hi@jasoncarloscox.com](mailto:hi@jasoncarloscox.com).
---
+++ conf-to-epub.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+#
+# conf-to-epub.sh -- convert a session of LDS General Conference to an epub file
+
+### PARSE ARGUMENTS ###
+
+year="$1"
+month="$2"
+language="${3:-eng}"
+
+if [ -z "$year" ] || [ -z "$month" ]; then
+    echo USAGE: "$0 <year> <month> [<language>]"
+    exit 1
+fi
+
+if [ "$(echo "$year" | grep -E -o '(19|20)[0-9][0-9]')" != "$year" ]; then
+    echo Invalid year
+    exit 1
+fi
+
+case "$month" in
+    april|April|apr|Apr|4|04)
+        month=04
+        month_name=April
+        ;;
+    october|October|oct|Oct|10)
+        month=10
+        month_name=October
+        ;;
+    *)
+        echo Invalid month
+        exit 1
+        ;;
+esac
+
+### SETUP VARIABLES ###
+
+base_url="https://www.churchofjesuschrist.org"
+conference_url="https://www.churchofjesuschrist.org/study/general-conference/$year/$month?lang=$language"
+build_dir="$(mktemp -d)"
+metadata_file="$build_dir/title.txt"
+build_files="$metadata_file"
+trap "rm -rf '$build_dir'" EXIT
+
+### LIST SESSIONS AND TALKS ###
+
+echo Getting list of talks
+talk_paths="$(
+    curl --silent --location "$conference_url" | \
+        htmlq --attribute href ".body nav a"
+)"
+
+if [ $? != 0 ]; then
+    echo Failed to get a list of talks
+    exit 1
+fi
+
+### OUTPUT METADATA ###
+
+echo Creating metadata
+cat >"$metadata_file" <<EOF
+---
+title: $month_name $year General Conference
+author: The Church of Jesus Christ of Latter-day Saints
+---
+EOF
+
+### GET SESSION AND TALK CONTENT ###
+
+echo Getting sessions and talks
+for talk_path in $talk_paths; do
+    echo Getting "$talk_path"
+    url="$base_url$talk_path"
+    name="$(echo "$talk_path" | grep -E -o '[^/]+$')"
+    html_file="$build_dir/$name-raw.html"
+    stage_file="$build_dir/$name-stage.html"
+
+    curl --silent --location "$url?lang=$language" > "$html_file"
+    title="$(htmlq --filename "$html_file" --text ".body h1")"
+    author="$(htmlq --filename "$html_file" --text ".body .author-name" | sed 's/By *//')"
+    author_role="$(htmlq --filename "$html_file" --text ".body .author-role")"
+    summary="$(htmlq --filename "$html_file" --text ".body .kicker")"
+
+    [ -n "$author" ] && title="$title ($author)"
+    [ -n "$author_role" ] && author="$author, $author_role"
+
+    echo "<h1>$title</h1>" >"$stage_file"
+    [ -n "$author" ] && echo "<p><em>$author</em></p>" >>"$stage_file"
+    [ -n "$summary" ] && echo "<p><em>$summary</em><p>" >>"$stage_file"
+
+    # only include body for talks, not sessions
+    if [ "${name%-session}" = "$name" ]; then
+        htmlq --filename "$html_file" ".body .body-block" >>"$stage_file"
+    fi
+
+    # building the epub from markdown works much better than HTML, so convert to
+    # markdown, removing footnotes and adding the hostname to link URLs
+    talk_file="$build_dir/$name.md"
+    pandoc -f html -t commonmark -o - "$stage_file" | \
+        sed \
+        -e 's#\[<sup>[0-9]\+</sup>\]([^)]*)##g' \
+        -e "s#\](/#]($base_url/#g" \
+        >"$talk_file"
+    build_files="$build_files $talk_file"
+done
+
+### CONVERT TO EPUB ###
+
+echo Converting to epub
+
+dir="conferences/$year/$month"
+mkdir -p "$dir"
+
+pandoc \
+    --epub-chapter-level 1 \
+    --toc --toc-depth 1 \
+    -o "$dir/general-conference-$year-$month-$language.epub" \
+    $build_files
